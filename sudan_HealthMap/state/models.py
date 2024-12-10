c2305160b5d9cdfc279e3b5bb37ec7fda32b484e
Ruba-Salih <rubasalih900@gmail.com>
2024-12-10 06:50:54 +0300

    update state model

--- sudan_HealthMap/state/models.py
+++ sudan_HealthMap/state/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class State(models.Model):
+    """
+    Model representing the sudan state.
+    """
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        """
+        String representation of the State instance.
+
+        Returns:
+            str: The name of the state.
+        """
+        return self.name
