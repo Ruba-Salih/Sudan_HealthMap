4351d58b1cb8b71013cdc83d0601ebc06a88d0ff
Ruba-Salih <rubasalih900@gmail.com>
2024-12-10 06:29:37 +0300

    update supervisor model

--- sudan_HealthMap/supervisor/models.py
+++ sudan_HealthMap/supervisor/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Supervisor(models.Model):
+    """
+    Model representing a Supervisor in the system.
+    
+    Attributes:
+        name (str): The full name of the supervisor.
+        email (str): The supervisor's unique email address, used for login and identification.
+        password (str): Hashed password for authentication purposes. 
+                        (Should be securely hashed before storage.)
+    """
+    name = models.CharField(max_length=255, help_text="Full name of the supervisor")
+    email = models.EmailField(unique=True, help_text="Unique email address for the supervisor")
+    password = models.CharField(max_length=128, help_text="Hashed password for the supervisor")
+
+    def __str__(self):
+        """
+        String representation of the Supervisor instance.
+
+        Returns:
+            str: The name of the supervisor.
+        """
+        return self.name
